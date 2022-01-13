577aef744a51ffd03f56e06a8b5157e22fe47075
Leonardo Demetrio <l.demetrio@accenture.com>
2022-01-12 21:38:18 -0300

    Pagination layout

---
+++ frontend/src/components/Pagination/index.tsx
@@ -0,0 +1,17 @@
+import {ReactComponent as ArrowIcon} from 'assets/img/arrow.svg';
+
+
+export default function Pagination() {
+    return (
+    <div className="dsmovie-pagination-container">
+        <div className="dsmovie-pagination-box">
+            <button className="dsmovie-pagination-button" disabled={true} >
+                <ArrowIcon />
+            </button>
+            <p>{`${1} de ${3}`}</p>
+            <button className="dsmovie-pagination-button" disabled={false} >
+                <ArrowIcon className="dsmovie-flip-horizontal" />
+            </button>
+        </div>
+    </div>);
+}
--- frontend/src/pages/Listing/index.tsx
+++ frontend/src/pages/Listing/index.tsx
@@ -1,6 +1,8 @@
-function Listing(){
-    return(
-        <h1>Página de listagem</h1>
+import Pagination from "components/Pagination";
+import './styles.css'
+function Listing() {
+    return (
+      <Pagination/>
     )
 }
 
